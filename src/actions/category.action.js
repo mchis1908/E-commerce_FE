37092a5d3516d066f95cc53ffc3d0398d722ea44
Thiện Khải <thienkhaiak5555@gmail.com>
2022-11-09 19:05:33 +0700

    add Categories Dropdown

---
+++ src/actions/category.action.js
@@ -0,0 +1,32 @@
+import axios from "../helpers/axios"
+import { categoryContants } from "./constants";
+
+
+export const getAllCategory = () => {
+    return async dispatch => {
+
+
+        dispatch({ type: categoryContants.GET_ALL_CATEGORIES_REQUEST });
+        const res = await axios.get(`category/getcategory`);
+        console.log(res);
+        if (res.status === 200) {
+
+            const { categoryList } = res.data;
+
+            dispatch({
+                type: categoryContants.GET_ALL_CATEGORIES_SUCCESS,
+                payload: { categories: categoryList }
+            });
+        }
+        else {
+            dispatch({
+                type: categoryContants.GET_ALL_CATEGORIES_FAILURE,
+                payload: { error: res.data.error }
+            });
+        }
+
+
+    }
+}
+
+
---
+++ src/components/MenuHeader/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import './style.css'
+import { getAllCategory } from '../../actions';
+
+const MenuHeader = props => {
+
+
+    const category = useSelector(state => state.category);
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        dispatch(getAllCategory());
+    }, []);
+
+
+    const renderCategories = (categories) => {
+        let mycategories = [];
+        for (let category of categories) {
+            mycategories.push(
+                <li key={category.name}>
+                    {
+                        category.parentId ? <a href={category.slug}> {category.name}</a> :
+                            <span>{category.name}</span>
+                    }
+                    {category.children.length > 0 ? (<ul>{renderCategories(category.children)}</ul>) : null}
+                </li>
+            );
+        }
+        return mycategories;
+    }
+
+    return (
+        <div className='menuHeader'>
+            <ul>
+                {category.categories.length > 0 ? renderCategories(category.categories) : null}
+            </ul>
+        </div>
+    )
+}
+
+
+
+export default MenuHeader
