bf21335c7374dd71c6ff6019ed2d7ead95a5222c
hungnongtrai <vuquochung2204@gmail.com>
2022-11-21 20:18:44 +0700

    Submit #2
    1. Cart

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import HomePage from './containers/HomePage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductListPage from './containers/ProductListPage';
-import { useDispatch, useSelector } from 'react-redux'
-import { isUserLoggedIn, updateCart } from './actions';
-import ProductDetailsPage from './containers/ProductDetailsPage';
-import CartPage from './containers/CartPage';
+import React, { useEffect } from "react";
+import "./App.css";
+import HomePage from "./containers/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListPage from "./containers/ProductListPage";
+import { useDispatch, useSelector } from "react-redux";
+import { isUserLoggedIn, updateCart } from "./actions";
+import ProductDetailsPage from "./containers/ProductDetailsPage";
+import CartPage from "./containers/CartPage";
 
 function App() {
-
-  const dispatch = useDispatch()
-  const auth = useSelector(state => state.auth)
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (!auth.authenticate) {
-      dispatch(isUserLoggedIn())
+      dispatch(isUserLoggedIn());
     }
-
-  }, [auth.authenticate])
-
+  }, [auth.authenticate]);
 
   useEffect(() => {
+    console.log("App.js - updateCart");
     dispatch(updateCart());
-  }, []);
+  }, [auth.authenticate]);
 
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path='/' element={<HomePage />} />
-          <Route exact path='/cart' element={<CartPage />} />
-          <Route exact path='/:productSlug/:productId/p' element={<ProductDetailsPage />} />
-          <Route exact path='/:slug' element={<ProductListPage />} />
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/cart" element={<CartPage />} />
+          <Route
+            exact
+            path="/:productSlug/:productId/p"
+            element={<ProductDetailsPage />}
+          />
+          <Route exact path="/:slug" element={<ProductListPage />} />
         </Routes>
       </Router>
-
     </div>
   );
 }
--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,13 +1,21 @@
-import axios from 'axios';
-import { api } from '../urlConfig';
+import axios from "axios";
+import store from "../store";
+import { api } from "../urlConfig";
 
-const token = window.localStorage.getItem('token');
+const token = window.localStorage.getItem("token");
 
 const axiosIntance = axios.create({
-    baseURL: api,
-    headers: {
-        'Authorization': token ? `Bearer ${token}` : ''
-    }
+  baseURL: api,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : "",
+  },
 });
 
+axiosIntance.interceptors.request.use((req) => {
+  const { auth } = store.getState();
+  if (auth.token) {
+    req.headers.Authorization = `Bearer ${auth.token}`;
+  }
+  return req;
+});
 export default axiosIntance;
